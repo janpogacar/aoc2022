8bb77f2bcedfcf861b355dd10cd6fea8d9c3939e
janpogacar <jan.pogachar@gmail.com>
2022-12-03 22:54:52 +0100

    learned up on sets, cleaned the code somewhat

--- day03/day3.py
+++ day03/day3.py
@@ -16,8 +16,9 @@ for x in rsDataList:
 
 result = 0
 for x in rsHalves:
-    # get a matching item in both halves (I don't fully understand sets
-    item = list(set(x[0]).intersection(x[1]))[0]
+    # get a matching item in both halves
+    # pop removes a random element, but the set should be one element long 
+    item = (set(x[0]).intersection(x[1])).pop()
     # Convert ASCII code to solution code
     if item.islower():
         result+=(ord(item)-ord('a')+1)
@@ -31,13 +32,11 @@ result = 0
 # Loop over input, increment loop counter by 3
 for i in range(0, len(rsDataList), 3):
     # loop over matching letters in first two stings
-    for x in (set(rsDataList[i]).intersection(rsDataList[i+1])):
-        # loop over matching characters, check if they are in third string
-        if x in rsDataList[i+2]:
-            # Convert ASCII code to solution code
-            if x.islower():
-                result+=(ord(x)-ord('a')+1)
-            else:
-                result+=(ord(x)-ord('A')+27)
+    x = ((set(rsDataList[i]).intersection(rsDataList[i+1])).intersection(rsDataList[i+2])).pop()
+    # Convert ASCII code to solution code
+    if x.islower():
+        result+=(ord(x)-ord('a')+1)
+    else:
+        result+=(ord(x)-ord('A')+27)
 
 print(f"Puzzle 2 result: {result}")
---
+++ day04/day04.py
@@ -0,0 +1,3 @@
+# opening the file in read mode
+myFile = open("input_04_1.txt", "r")
+#myFile = open("demoinput_04.txt", "r") # uncomment for demo input
