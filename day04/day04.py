63025b378affaed67df1e21cfa83d58619d7e3ce
janpogacar <jan.pogachar@gmail.com>
2022-12-04 10:31:33 +0100

    Day 4 solution added

--- day04/day04.py
+++ day04/day04.py
@@ -1,3 +1,26 @@
 # opening the file in read mode
 myFile = open("input_04_1.txt", "r")
-#myFile = open("demoinput_04.txt", "r") # uncomment for demo input
+
+# reading the file
+rsData = myFile.read()
+  
+# split file at new lines
+rsDataList = rsData.split("\n")
+
+result0 = 0
+result1 = 0
+for x in rsDataList:
+    # extract coordinates
+    a, b = x.split(",")
+    a1, a2 = a.split("-")
+    b1, b2 = b.split("-")
+    # check for total overlap
+    if (int(a1) <= int(b1) and int(a2) >= int(b2)) or (int(b1) <= int(a1) and int(b2) >= int(a2)):
+        result0 += 1
+        result1 += 1
+    # check for partial overlap
+    elif (int(b1) >= int(a1) and int(b1) <= int(a2)) or (int(b2) >= int(a1) and int(b2) <= int(a2)):
+        result1 += 1
+
+print(f"Puzzle 1 result: {result0}")
+print(f"Puzzle 2 result: {result1}")
