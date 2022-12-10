dbda625f1c2d16b56dbbea531659762d8e9116b0
janpogacar <jan.pogachar@gmail.com>
2022-12-10 16:23:06 +0100

    Day 10 code cleanup

--- day10/Day10.py
+++ day10/Day10.py
@@ -1,3 +1,16 @@
+def logResult(xReg, rValues, pc):
+    # part 1 solution reg
+    global rList
+    # 1-D CRT register for part 2
+    global crt
+    # Check if part 1 solution
+    if (pc+1 in rValues):
+        rList.append(xReg)
+    # Check if part 2 solution
+    if pc % 40 in [xReg-1, xReg, xReg+1]:
+        crt[pc] = "#"
+    return
+
 # opening the file in read mode
 myFile = open("input_10_1.txt", "r")
 #myFile = open("demoinput10.txt", "r")
@@ -6,58 +19,42 @@ rsData = myFile.read()
 # split file at new lines
 rsDataList = rsData.split("\n")
 
-# procgram counter
-pc = 1
-# register x
-xReg = 1
-# Results register
-rList = []
-rValues = [20, 60, 100, 140, 180, 220]
-
-for x in rsDataList:
-    if x == "noop":
-        if (pc in rValues):
-            rList.append(xReg)
-        pc += 1
-    else:
-        a,b = x.split(" ")
-        if (pc in rValues):
-            rList.append(xReg)
-        pc += 1
-        if (pc in rValues):
-            rList.append(xReg)
-        xReg += int(b)
-        pc += 1
-
-print(f"Puzzle 1 result: {sum(a*b for a,b in zip(rList,rValues))}")
-
-# procgram counter
+# program counter
 pc = 0
 # register x
 xReg = 1
-# Results register
+# Results register for part 1
 rList = []
+# Result indices and multipliers
 rValues = [20, 60, 100, 140, 180, 220]
 
+# 1-D CRT register for part 1
 crt = []
+# Fill CRT with emtpy dots
 for i in range(6*40):
     crt.append('.')
 
 for i, x in enumerate(rsDataList):
+    # No operation
     if x == "noop":
-        if pc % 40 in [xReg-1, xReg, xReg+1]:
-            crt[pc] = "#"
+        logResult(xReg, rValues, pc)
+        # Increment program counter
         pc += 1
+    # Adition
     else:
-        a,b = x.split(" ")
-        if pc % 40 in [xReg-1, xReg, xReg+1]:
-            crt[pc] = "#"
+        # Split line to get adder
+        b = x.split(" ")[1]
+        logResult(xReg, rValues, pc)
+        # Increment program counter for the first time
         pc += 1
-        if pc % 40 in [xReg-1, xReg, xReg+1]:
-            crt[pc] = "#"
+        logResult(xReg, rValues, pc)
+        # Make the addition
         xReg += int(b)
+        # Increment program counter for the second time time
         pc += 1
 
+print(f"Puzzle 1 result: {sum(a*b for a,b in zip(rList,rValues))}")
 print("Puzzle 2 result:")
+# Print 2-D array
 for i in range(0, len(crt), 40):
     print("".join(crt[i:i+40]))
