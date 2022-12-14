2227392f9d0beb6b61190f53e7ee06b06072bf09
Jan Pogacar <jan.pogacar@renishaw.com>
2022-12-14 11:59:22 +0100

    Cleaned up solution for day 14

--- day14/day14.py
+++ day14/day14.py
@@ -1,7 +1,7 @@
 import copy
 
-#myFile = open("input_14_1.txt", "r")
-myFile = open("demoinput14.txt", "r")
+myFile = open("input_14_1.txt", "r")
+#myFile = open("demoinput14.txt", "r")
 # reading the file
 rsData = myFile.read()
 # split file at new lines
@@ -20,26 +20,28 @@ for x in coords:
     for i in range(len(x)-1):
         x1, y1= eval(x[i])
         x2, y2= eval(x[i+1])
-
-        if (x1==x2) and (y1==y2):
-            if [x1, y1] not in rocks:
-                rocks.append([x1, y1])
-                if y1 > maxY:
-                    maxY = y1
-        elif (x1==x2):
+        # draw lines in y dimension
+        if (x1==x2):
             llist = [y1, y2]
+            # ensure correct order when looping
             llist.sort()
             for j in range(llist[0], llist[1]+1):
+                # Avoid duplicates
                 if [x1, j] not in rocks:
                     rocks.append([x1, j])
+                    # find max Y for part 2
                     if j > maxY:
                         maxY = j
+        # draw lines in x dimension
         else:
             llist = [x1, x2]
+            # ensure correct order when looping
             llist.sort()
             for j in range(llist[0], llist[1]+1):
+                # Avoid duplicates
                 if [j, y1] not in rocks:
                     rocks.append([j, y1])
+                    # find max Y for part 2
                     if y1 > maxY:
                         maxY = y1
 
@@ -47,14 +49,19 @@ rocks2 = copy.deepcopy(rocks)
 maxY += 2
 count = 0
 sandUnit = 0
-while(count < 1000):
+lsp = []
+# if sand falls beyond part 2 floor, it is deemed to be infinitely cascading
+while(count < maxY):
     if ([sand[0], sand[1]+1] not in rocks):
+        lsp = sand.copy()
         sand = [sand[0], sand[1]+1]
         count += 1
     elif [sand[0]-1, sand[1]+1] not in rocks:
+        lsp = sand.copy()
         sand = [sand[0]-1, sand[1]+1]
         count += 1
     elif [sand[0]+1, sand[1]+1] not in rocks:
+        lsp = sand.copy()
         sand = [sand[0]+1, sand[1]+1]
         count += 1
     else:
@@ -68,23 +75,29 @@ print(f"Part 1 solution: {sandUnit}")
 count = 0
 sandUnit = 1
 sand = [500,0]
+# create stack of sand particles, for quicker solving
+sandList = []
 
-while(count < 1000):
+# Give this part some time to compute, at least 30 seconds
+while(1):
     if ([sand[0], sand[1]+1] not in rocks2) and sand[1]+1 != maxY:
+        # add previous position to stack
+        sandList.append(sand)
         sand = [sand[0], sand[1]+1]
-        count += 1
     elif ([sand[0]-1, sand[1]+1] not in rocks2) and sand[1]+1 != maxY:
+        sandList.append(sand)
         sand = [sand[0]-1, sand[1]+1]
-        count += 1
     elif ([sand[0]+1, sand[1]+1] not in rocks2)  and sand[1]+1 != maxY:
+        sandList.append(sand)
         sand = [sand[0]+1, sand[1]+1]
-        count += 1
     else:
         if sand == [500, 0]:
             break
         rocks2.append(sand)
-        sand = [500, 0]
+        # assign last item to sand
+        sand = sandList[-1]
+        # remove last item from stack
+        sandList.pop()
         sandUnit += 1
-        count = 0
 
 print(f"Part 2 solution: {sandUnit}")
